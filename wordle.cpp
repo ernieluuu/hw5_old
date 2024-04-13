06bd907505045155b81f53cef2ce4860a214d6bd
Ernest Lu <eluindustries@gmail.com>
2024-04-12 20:07:07 -0700

    pseudocode + ideation

--- wordle.cpp
+++ wordle.cpp
@@ -23,6 +23,21 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+
+	/*
+	build it up 1 letter at a time
+	storing and updating a set of 
+	"possibles"	
+
+	// dict is passed as reference because it's too big to make a copy of each time
+	// could you keep popping out of dict? So it's smaller each time you pass it.
+	// But then you would lose the dict from posterity.
+
+	// where to store the set?
+
+	*/
+
+
 }
 
 // Define any helper functions here
