0e1bb6b0b8c6a1c45b654fcbfc27e2c0c3333709
Ernest Lu <eluindustries@gmail.com>
2024-04-19 21:22:48 -0700

    got schedwork working

--- schedwork.cpp
+++ schedwork.cpp
@@ -7,65 +7,132 @@
 #include <map>
 #include <algorithm>
 // add or remove necessary headers as you please
-
 #endif
 
 #include "schedwork.h"
-
 using namespace std;
 
-// a constant that can be used to indicate an INVALID 
+// a constant that can be used to indicate an INVALID
 // worker ID if that is useful to your implementation.
 // Feel free to not use or delete.
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
-
 // Add prototypes for any helper functions here
-// bool is_valid(/*arguments*/);
-bool schedule_helper(
+bool checkPossibleSchedule(const AvailabilityMatrix& avail, size_t day, size_t workerIndex);
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts,
+	DailySchedule& schedule, size_t day, size_t shiftCount, vector<int> shifts, int workerIndex);
+
+// Add your implementation of schedule() and other helper functions here
+bool schedule(
 	const AvailabilityMatrix& avail,
 	const size_t dailyNeed,
 	const size_t maxShifts,
-	DailySchedule& sched,
-	unsigned int day = 0,
-	std::vector<size_t> shiftsAssigned = {}
-);
-
-// Add your implementation of schedule() and other helper functions here
-
-bool schedule(
-    const AvailabilityMatrix& avail,
-    const size_t dailyNeed,
-    const size_t maxShifts,
-    DailySchedule& sched
-)
+	DailySchedule& sched
+) 
 {
-    if(avail.size() == 0U){
-        return false;
-    }
-    sched.clear();
-    // Add your code below
+	bool availIsEmpty = (avail.size() == 0U);
+	if (availIsEmpty) {
+		return false;
+	}
 
-	// calls schedule_helper
-	return schedule_helper(avail, dailyNeed, maxShifts, sched);
+	sched.clear();
 
-	/*
-	
-	iterate through avail using a for loop + iterator
-	if condition,
-	backtrack
+	// Add your code below
+	int workerIndex = 0;
+	vector<int> shifts;
 
+	/*initialization*/
+	for (int i = 0; i < avail[0].size(); i++) {
+		shifts.push_back(0);
+	}
 
-	sets worker 1 day 1 to working, continues through algo
-	with setting to working first and then backtracking (are they default to non working?
-	or do we have to manually set it to non working?)
-	and continue through the algorithm.
+	for (int i = 0; i < 10; i++) {
+		checkPossibleSchedule(avail, 0, workerIndex);
+	}
 
+	// resize the matrix to the same as available size
+	sched.resize(avail.size());
 
-	
-	*/
+	checkPossibleSchedule(avail, 0, 0);
 
+	return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, shifts, workerIndex);
+}
 
+// Helper function to check if a solution is possible given the initializations
+bool checkPossibleSchedule(const AvailabilityMatrix& avail, size_t day, size_t workerIndex) {
+	// Base case: if all days are processed, return true
+	if (day == avail.size()) {
+		return true;
+	}
+
+	// Recursive case: process the current day and move to the next day
+	if (workerIndex < avail[day].size()) {
+		// Recursive call to process the next worker
+		if (checkPossibleSchedule(avail, day, workerIndex + 1)) {
+			return true;
+		}
+	}
+	else {
+		// All workers processed for the current day, move to the next day
+		// Recursive call to process the next day
+		if (checkPossibleSchedule(avail, day + 1, 0)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// Helper function to check if a worker has exceeded the maximum number of shifts
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts,
+	DailySchedule& schedule, size_t day, size_t shiftCount, vector<int> shifts, int workerIndex) 
+{
+
+	// if all of the days are filled
+	bool allDaysFilled = (day == avail.size());
+	if (allDaysFilled) {
+		return true;
+	}
+
+	bool shiftCountReached = (shiftCount == dailyNeed);
+	if (shiftCountReached) {
+		return scheduleHelper(avail, dailyNeed, maxShifts, schedule, day + 1, 0, shifts, 0);
+	}
+
+	bool lastWorkerReached = (workerIndex == avail[0].size());
+	if (lastWorkerReached) {
+		return false;
+	}
+
+	bool workerAvailable = avail[day][workerIndex];
+	bool shiftsRemaining = (shifts[workerIndex] < maxShifts);
+
+	if (workerAvailable && shiftsRemaining) {
+		// assigns worker & updates shift
+		schedule[day].push_back(workerIndex);
+		shiftCount++;
+		shifts[workerIndex]++;
+
+		// moves onto next day
+		if (scheduleHelper(avail, dailyNeed, maxShifts, schedule, day, shiftCount, shifts, workerIndex + 1)) {
+			return true;
+		}
+
+		schedule[day].pop_back();
+		shiftCount--;
+		shifts[workerIndex]--;
+
+		if (scheduleHelper(avail, dailyNeed, maxShifts, schedule, day, shiftCount, shifts, workerIndex + 1)) {
+			return true;
+		}
+	}
+	else {
+		if (scheduleHelper(avail, dailyNeed, maxShifts, schedule, day, shiftCount, shifts, workerIndex + 1)) {
+			return true;
+		}
+	}
+
+	return false;
 }
 
 /*bool schedule_helper(
@@ -87,50 +154,3 @@ bool schedule(
 	// always assume you can schedule them on that day, and change your answer if you return false
 
 }*/
-
-bool schedule_helper(
-	const AvailabilityMatrix& avail,
-	const size_t dailyNeed,
-	const size_t maxShifts,
-	DailySchedule& sched,
-	unsigned int day,
-	std::vector<size_t> shiftsAssigned
-) {
-	const size_t numDays = avail[0].size();
-	const size_t numWorkers = avail.size();
-
-	if (shiftsAssigned.empty()) {
-		shiftsAssigned.assign(numWorkers, 0);
-	}
-
-	if (day == numDays) {
-		return true;  // All days scheduled successfully
-	}
-
-	if (sched.size() <= day) {
-		sched.emplace_back(dailyNeed, INVALID_ID);
-	}
-
-	for (Worker_T worker = 0; worker < numWorkers; ++worker) {
-		if (avail[worker][day] && shiftsAssigned[worker] < maxShifts) {
-			sched[day][sched[day].size() - dailyNeed] = worker;
-			shiftsAssigned[worker]++;
-
-			if (schedule_helper(avail, dailyNeed, maxShifts, sched, day + 1, shiftsAssigned)) {
-				return true;
-			}
-
-			sched[day][sched[day].size() - dailyNeed] = INVALID_ID;
-			shiftsAssigned[worker]--;
-		}
-	}
-
-	return false;  // No valid schedule found for this day
-}
-
-/*
-
-
-
-
-*/
