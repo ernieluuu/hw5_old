330f752f8737e2a223f71e26dba406a4a485e80b
Ernest Lu <eluindustries@gmail.com>
2024-04-19 19:31:08 -0700

    wordle works, schedwork doesn't

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,15 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+// bool is_valid(/*arguments*/);
+bool schedule_helper(
+	const AvailabilityMatrix& avail,
+	const size_t dailyNeed,
+	const size_t maxShifts,
+	DailySchedule& sched,
+	unsigned int day = 0,
+	std::vector<size_t> shiftsAssigned = {}
+);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -38,8 +46,91 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+	// calls schedule_helper
+	return schedule_helper(avail, dailyNeed, maxShifts, sched);
 
+	/*
+	
+	iterate through avail using a for loop + iterator
+	if condition,
+	backtrack
+
+
+	sets worker 1 day 1 to working, continues through algo
+	with setting to working first and then backtracking (are they default to non working?
+	or do we have to manually set it to non working?)
+	and continue through the algorithm.
+
+
+	
+	*/
 
 
 }
 
+/*bool schedule_helper(
+	const AvailabilityMatrix& avail,
+	const size_t dailyNeed,
+	const size_t maxShifts,
+	DailySchedule& sched,
+	unsigned int day = 0
+)
+{
+
+	// if it's 0, move on
+	// if it's one u can schedule or not scheduel
+	// if they are 1 you update the sched
+	// move on in a backtracing way
+	// return true if curr idx is valid and it's the last idx of the matrix.
+	// return false if you get to the end of a row and you haven't gotten enough workers
+	// chain back through recursive calls to update your answer
+	// always assume you can schedule them on that day, and change your answer if you return false
+
+}*/
+
+bool schedule_helper(
+	const AvailabilityMatrix& avail,
+	const size_t dailyNeed,
+	const size_t maxShifts,
+	DailySchedule& sched,
+	unsigned int day,
+	std::vector<size_t> shiftsAssigned
+) {
+	const size_t numDays = avail[0].size();
+	const size_t numWorkers = avail.size();
+
+	if (shiftsAssigned.empty()) {
+		shiftsAssigned.assign(numWorkers, 0);
+	}
+
+	if (day == numDays) {
+		return true;  // All days scheduled successfully
+	}
+
+	if (sched.size() <= day) {
+		sched.emplace_back(dailyNeed, INVALID_ID);
+	}
+
+	for (Worker_T worker = 0; worker < numWorkers; ++worker) {
+		if (avail[worker][day] && shiftsAssigned[worker] < maxShifts) {
+			sched[day][sched[day].size() - dailyNeed] = worker;
+			shiftsAssigned[worker]++;
+
+			if (schedule_helper(avail, dailyNeed, maxShifts, sched, day + 1, shiftsAssigned)) {
+				return true;
+			}
+
+			sched[day][sched[day].size() - dailyNeed] = INVALID_ID;
+			shiftsAssigned[worker]--;
+		}
+	}
+
+	return false;  // No valid schedule found for this day
+}
+
+/*
+
+
+
+
+*/
--- wordle.cpp
+++ wordle.cpp
@@ -29,6 +29,7 @@ using namespace std;
 using namespace std;
 
 // Add prototypes of helper functions here
+bool is_valid_prefix(const std::string& prefix, const std::set<std::string>& dict);
 void findWords(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& potentialWords, string& curr, int currLoc);
 bool isWord(const std::string& word, const std::set<std::string>& dict);
 
@@ -42,27 +43,38 @@ std::set<std::string> wordle(const std::string& in, const std::string& floating,
         return potentialWords;
     }
 
-    // START: Added code
     string curr = in;
     findWords(in, floating, dict, potentialWords, curr, 0);
-    // END: Added code
 
     return potentialWords;
 }
 
+
+
 // Define any helper functions here
+bool is_valid_prefix(const std::string& prefix, const std::set<std::string>& dict) {
+    auto it = dict.lower_bound(prefix);
+    return it != dict.end() && it->substr(0, prefix.size()) == prefix;
+}
+
+
 /*
  * nvm, scratch all of that you first generate a set / list of possible words and then use the find() method of a set (logn) to check if it is a real word in the english langauge.
  * check if in[currLoc] == '-' or == alphabet_loc
  */
 /*this function recursively generates a set of potential letter combinations*/
-void findWords(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& potentialWords, string& curr, int currLoc) {
-    /* where currLoc stands for the current location in 'in' */
+void findWords(const std::string& in, const std::string& floating, const std::set<std::string>& dict, std::set<std::string>& potentialWords, string& curr, int currLoc)
 
-    // START: Added code
+/*
+{
+    // where currLoc stands for the current location in 'in'
+
     // Base case: if all letters are filled, check if it's a valid word
     if (currLoc == in.size()) {
-        if (isWord(curr, dict) && std::none_of(floating.begin(), floating.end(), [&curr](char c) { return curr.find(c) == std::string::npos; })) {
+        // Check if the current word is a valid word and all floating letters are used
+        if (isWord(curr, dict) && all_of(floating.begin(), floating.end(), [&curr](char c) {
+            return curr.find(c) != string::npos;
+        })) {
             potentialWords.insert(curr);
         }
         return;
@@ -85,12 +97,57 @@ void findWords(const std::string& in, const std::string& floating, const std::se
 
     // Try placing all alphabet letters at the current location
     for (char c = 'a'; c <= 'z'; c++) {
+        // Optimization: Skip alphabet letters that are in the floating string
+        if (floating.find(c) == string::npos) {
+            curr[currLoc] = c;
+            // Optimization: Check if the current prefix is valid before recursing
+            if (is_valid_prefix(curr.substr(0, currLoc + 1), dict)) {
+                findWords(in, floating, dict, potentialWords, curr, currLoc + 1);
+            }
+            curr[currLoc] = '-'; // Backtrack
+        }
+    }
+}
+*/
+
+{
+    /* where currLoc stands for the current location in 'in' */
+
+    // Base case: if all letters are filled, check if it's a valid word
+    if (currLoc == in.size()) {
+        // Check if the current word is a valid word and all floating letters are used
+        if (isWord(curr, dict) && all_of(floating.begin(), floating.end(), [&curr, &floating](char c) {
+            return std::count(curr.begin(), curr.end(), c) >= std::count(floating.begin(), floating.end(), c);
+        })) {
+            potentialWords.insert(curr);
+        }
+        return;
+    }
+
+    // If the current location is a fixed letter, move to the next location
+    if (in[currLoc] != '-') {
+        findWords(in, floating, dict, potentialWords, curr, currLoc + 1);
+        return;
+    }
+
+    // Try placing floating letters at the current location
+    for (char c : floating) {
         curr[currLoc] = c;
         findWords(in, floating, dict, potentialWords, curr, currLoc + 1);
         curr[currLoc] = '-'; // Backtrack
     }
-    // END: Added code
+
+    // Try placing all alphabet letters at the current location
+    for (char c = 'a'; c <= 'z'; c++) {
+        curr[currLoc] = c;
+        // Optimization: Check if the current prefix is valid before recursing
+        if (is_valid_prefix(curr.substr(0, currLoc + 1), dict)) {
+            findWords(in, floating, dict, potentialWords, curr, currLoc + 1);
+        }
+        curr[currLoc] = '-'; // Backtrack
+    }
 }
+
 
 bool isWord(const std::string& word, const std::set<std::string>& dict) {
     return dict.find(word) != dict.end();
